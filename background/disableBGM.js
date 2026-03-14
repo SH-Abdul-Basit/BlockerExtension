e126b9c44245e740a9232581f6c8f5bf6c90e973
Y2kSurviver <abasitmarkting181@gmail.com>
2026-03-14 23:16:52 +0500

    Now music stops when user switches to different window and update styling of popup.

--- background/disableBGM.js
+++ background/disableBGM.js
@@ -1,12 +1,18 @@
-const handleDisableBDM = async (activeInfo) => {
+const handleTabChange = async (activeInfo) => {
     const checkedBGB = JSON.parse(localStorage.getItem("disableBGM"));
     if (checkedBGB) {
         const prevTab = await browser.tabs.get(activeInfo.previousTabId);
         if (checkBlockedWord(prevTab.url, "youtube")) {
             console.log("Sending...");
-            await browser.tabs.sendMessage(prevTab.id, { type: "send_video" });
+            browser.tabs.sendMessage(prevTab.id, { type: "send_video" });
         }
     }
 };
 
-browser.tabs.onActivated.addListener(handleDisableBDM);
+browser.tabs.onActivated.addListener(handleTabChange);
+
+// const handleWindowChange = () => {
+//     console.log("Window Changed");
+// };
+
+// browser.windows.onFocusChanged.addListener(handleWindowChange);
--- content/disableBGM.js
+++ content/disableBGM.js
@@ -1,12 +1,24 @@
+// Currently only works for youtube
 console.log("Disable youtube music");
+
+const pauseVideo = () => {
+    let videos = document.getElementsByTagName("video");
+    videos = Array.from(videos);
+    videos[0].pause();
+};
 
 browser.runtime.onMessage.addListener((request) => {
     if (request.type == "send_video") {
-        let videos = document.getElementsByTagName("video");
-        videos = Array.from(videos);
-        videos[0].pause();
+        pauseVideo();
         // for (let video in videos) {
         //     console.log(video);
         // }
     }
 }); 
+
+window.addEventListener("blur", () => {
+    const checkedBGB = JSON.parse(localStorage.getItem("disableBGM"));
+    if (checkedBGB) {
+        pauseVideo();
+    }
+});
